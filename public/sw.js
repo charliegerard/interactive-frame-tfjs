fd47568ca478e240509f24198b98ead5db4bae08
Charlie Gerard <annecharlotte.gerard@gmail.com>
2021-07-23 21:58:20 +0200

    test move PWA files

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", function (e) {
+  console.log("boop");
+  e.waitUntil(
+    caches.open("sw-cache").then(function (cache) {
+      return cache.add("index.html");
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match("event.request").then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});

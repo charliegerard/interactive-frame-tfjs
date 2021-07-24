f945da73cd2c4e8a4e8351aef214c8690ae4b7f9
Charlie Gerard <annecharlotte.gerard@gmail.com>
2021-07-24 11:32:01 +0200

    depth test

--- src/App.js
+++ src/App.js
@@ -91,9 +91,9 @@ async function init() {
   topLeftCorner = new THREE.Vector3();
 
   if (touchscreen) {
-    bottomRightCorner.set(50.0, -0.0, -10.0);
-    bottomLeftCorner.set(-50.0, -0.0, -10.0);
-    topLeftCorner.set(-50.0, 100.0, -10.0);
+    bottomRightCorner.set(50.0, -0.0, -30.0);
+    bottomLeftCorner.set(-50.0, -0.0, -30.0);
+    topLeftCorner.set(-50.0, 100.0, -30.0);
   } else {
     bottomRightCorner.set(50.0, -0.0, -30.0);
     bottomLeftCorner.set(-50.0, -0.0, -30.0);
